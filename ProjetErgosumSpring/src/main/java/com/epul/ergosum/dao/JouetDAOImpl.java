a343e6836c0ae8a57a73a8063ce3d6524e8f056f
Laura <premillieu.laura@gmail.com>
2015-04-19 16:53:36 +0200

    JouetDAOImpl : correction connexionclosed

--- ProjetErgosumSpring/src/main/java/com/epul/ergosum/dao/JouetDAOImpl.java
+++ ProjetErgosumSpring/src/main/java/com/epul/ergosum/dao/JouetDAOImpl.java
@@ -10,7 +10,7 @@ import com.epul.ergosum.model.Jouet;
 
 public class JouetDAOImpl implements JouetDAO {
 
-	private SessionFactory sessionFactory;
+	private SessionFactory	sessionFactory;
 
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
@@ -22,32 +22,30 @@ public class JouetDAOImpl implements JouetDAO {
 
 		Session currentSession = this.sessionFactory.openSession();
 		Query query = currentSession.createQuery("from Jouet");
+		List<Jouet> result = query.list();
 		currentSession.close();
 
-		return query.list();
+		return result;
 	}
 
 	@Override
 	public Jouet getJouet(String id) {
 
 		Session currentSession = this.sessionFactory.openSession();
-		Query query = currentSession.createQuery(
-				"from Jouet where NUMERO = :identifier ").setParameter(
-				"identifier", id);
+		Query query = currentSession.createQuery("from Jouet where NUMERO = :identifier ").setParameter("identifier", id);
+		Jouet result = (Jouet) query.uniqueResult();
 		currentSession.close();
 
-		return (Jouet) query.uniqueResult();
+		return result;
 	}
 
 	@Override
 	public int addJouet(Jouet jouet) {
 		Session currentSession = this.sessionFactory.openSession();
-		Query query = currentSession
-				.createQuery("insert into Jouet(numero, codecateg, codetranche, libelle)");
+		Query query = currentSession.createQuery("insert into Jouet(numero, codecateg, codetranche, libelle)");
 		query.setParameter("numero", jouet.getNumero());
 		query.setParameter("codecateg", jouet.getCategorie().getCodecateg());
-		query.setParameter("codetranche", jouet.getTrancheage()
-				.getCodetranche());
+		query.setParameter("codetranche", jouet.getTrancheage().getCodetranche());
 		query.setParameter("libelle", jouet.getLibelle());
 		int result = query.executeUpdate();
 		currentSession.close();
@@ -63,8 +61,7 @@ public class JouetDAOImpl implements JouetDAO {
 	@Override
 	public int suppressJouet(String id) {
 		Session currentSession = this.sessionFactory.openSession();
-		Query query = currentSession
-				.createQuery("delete Jouet where NUMERO = :identifier");
+		Query query = currentSession.createQuery("delete Jouet where NUMERO = :identifier");
 		query.setParameter("identifier", id);
 		int result = query.executeUpdate();
 		currentSession.close();
